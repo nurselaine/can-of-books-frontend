4046e400a78e00b36be58e0d26001321891d29a2
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2022-06-27 18:36:33 -0700

    added about us, bestbooks, header,  and refactored app.js

--- src/About.js
+++ src/About.js
@@ -1,10 +1,37 @@
+import React from "react";
 import { Component } from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class Profile extends Component {
 
   render() {
     /* TODO: render information about the developers */
-    return <p>Profile page coming soon</p>
+    return (
+      <>
+      <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>Elaine Huynh</Card.Title>
+    <Card.Text>
+    Hello! My name is Elaine Huynh, I am a critical care nurse in the process of finding a new career as a software engineer. Having graduated from Seattle Pacific University in 2020, I continued my education at Bellevue College to gain a foundation in Java. Currently, I am a student at Code Fellows working on front-end development, and soon to be designing full-stack web applications.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="./images/Stephen.jpeg" />
+  <Card.Body>
+    <Card.Title>Stephen Martinez</Card.Title>
+    <Card.Text>
+    Hello, my name is Stephen Martinez.  Before my journey here at Code Fellows, I was in the security industry.  Most recently I worked for Highline School District where I was a safety and security officer as well as a high school baseball and wrestling coach.  I was injured on the job and it got me thinking of what I could do while I was at home healing.  A family member told me I should look into what he does, which is software development.  I was very interested and when he showed me what he does and what he creates, I was hooked.  Ideally, I'd like to work in an environment where I'm helping people with the new skills that I'm learning.  I think my drive to help the people around me and my work ethic will set me in the right direction in my goals to becoming a successful Developer.
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
+</>
+    )
   }
 };
 
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel'
+
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,17 +12,48 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  async componentDidMount() {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books`;
+      let bookData = await axios.get(url);
+      if (bookData.data) {
+        this.setState({
+          books: bookData.data
+        })
+      }
+    } catch (err) {
+      console.log(err, 'An error occured');
+    }
+  }
 
   render() {
-
+    console.log(this.state.books, 'books');
     /* TODO: render all the books in a Carousel */
-
+    let renderedBooks = this.state.books.map((book, idx) => {
+      return (
+        <Carousel.Item key={idx}>
+          <img
+            className="d-block w-100"
+            src="./images/library.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>{book.title}</h3>
+            <p>{book.description}</p>
+            <p>{book.genre}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      )
+    })
     return (
       <>
+
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+          <Carousel>
+            {renderedBooks}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
